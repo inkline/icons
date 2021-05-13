4913d920432e80d8ee93443bfccaa5313e9b8e2b
Alex Grozav <alex@grozav.com>
2021-05-13 14:31:45 +0300

    chore: Changed prepare script to prepublishOnly

--- build/plugin.d.ts
+++ build/plugin.d.ts
@@ -1,6 +1,12 @@
 import { Plugin } from 'vue';
+import { Svg } from './types';
+export declare type InklineIconsPlugin = Plugin & {
+    add(icons: {
+        [key: string]: Svg;
+    }): void;
+};
 export interface PluginOptions {
     registerComponent?: boolean;
 }
-export declare const InklineIcons: Plugin;
+export declare const InklineIcons: InklineIconsPlugin;
 export default InklineIcons;
