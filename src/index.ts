ea42ecc4ce057b7a2bbc3182339e355653c2e3d5
Alex Grozav <alex@grozav.com>
2022-01-03 16:51:37 +0200

    feat: Exporting inkline icons pack separately and components in main file.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export * from './config';
-export * from './plugin';
-export * from './packs';
+import * as inklineIconsPack from '@inkline/icons/packs/inkline';
+
+export { inklineIconsPack };
+export * from '@inkline/icons/config';
+export * from '@inkline/icons/components';
+export * from '@inkline/icons/controllers';
+export * from '@inkline/icons/helpers';
+export * from '@inkline/icons/plugin';
+export * from '@inkline/icons/packs';
